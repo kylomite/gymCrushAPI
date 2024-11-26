e86d311bfdda4f2bef0a1e55ae4947151b1a300f
Kyle Delaney <khdelaney99@gmail.com>
2024-11-26 08:02:10 -0500

    add single user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!(
+  first_name: "John",
+  last_name: "Doe",
+  email: "john.doe@example.com",
+  password: "password123"
+#   sex: "male",
+#   weight_lbs: 180.5,
+#   height_ft: 5,
+#   height_in: 10,
+#   age: 30,
+#   activity_level: 3, 
+#   target_calories: 2500,
+#   target_fats: 70,
+#   target_carbs: 300,
+#   target_protein: 150
+)
+
+
