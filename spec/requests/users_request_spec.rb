a24c4083f9ba33e023c4a27c1e119e9128a21d79
Kyle Delaney <khdelaney99@gmail.com>
2024-11-26 08:13:47 -0500

    add structure for user controller and request spec

---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET all users" do
+    describe "HAPPY path" do
+
+    end
+
+    describe "SAD path" do
+      
+    end
+  end
+
+  describe "GET single users" do
+    describe "HAPPY path" do
+
+    end
+
+    describe "SAD path" do
+      
+    end
+  end
+
+  describe "PATCH single users" do
+    describe "HAPPY path" do
+
+    end
+
+    describe "SAD path" do
+      
+    end
+  end
+
+  describe "DELETE single users" do
+    describe "HAPPY path" do
+
+    end
+
+    describe "SAD path" do
+      
+    end
+  end
+end
