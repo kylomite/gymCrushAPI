6830f6f525fff8d0c53486bf3895ce688f71c00a
Kyle Delaney <khdelaney99@gmail.com>
2024-11-26 08:06:38 -0500

    add new user seed, and user serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,21 @@ User.create!(
 #   target_protein: 150
 )
 
+User.create!(
+  first_name: "Kyle",
+  last_name: "Lastname",
+  email: "kyle.lastname@example.com",
+  password: "password"
+#   sex: "male",
+#   weight_lbs: 180.5,
+#   height_ft: 5,
+#   height_in: 10,
+#   age: 30,
+#   activity_level: 3, 
+#   target_calories: 2500,
+#   target_fats: 70,
+#   target_carbs: 300,
+#   target_protein: 150
+)
+
 
