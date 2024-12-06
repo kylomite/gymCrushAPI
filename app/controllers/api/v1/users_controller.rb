ba19800e6a3ce8261211eda326993bbcaacbea1d
Kyle Delaney <khdelaney99@gmail.com>
2024-12-06 09:43:05 -0500

    update user verification

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: [:login]
+  skip_before_action :verify_authenticity_token
 
 
   def index
